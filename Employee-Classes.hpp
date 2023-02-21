2c036e90576a088e1d74b7042e9c779cc6496aa2
Stephen Cowan <stephencowan0@gmail.com>
2023-02-20 17:41:58 -0700

    first set of inhereited employee class types

--- Employee-Classes.hpp
+++ Employee-Classes.hpp
@@ -7,7 +7,7 @@ using namespace std;
 
 class Employee
 {
-private:
+protected:
     string name;
     string position;
     string position_description;
@@ -74,5 +74,84 @@ public:
         cout << "Subordinates: " << subordinates << endl;
         cout << "Department: " << department << endl;
         cout << "Level: " << level << endl;
+    }
+};
+
+class CEO : Employee
+{
+protected:
+    vector<Store_Manager> *subordinates;
+
+public:
+    CEO(string new_name, string new_position, string new_position_description, int new_salary, string new_department, int new_level)
+    {
+        name = new_name;
+        position = new_position;
+        position_description = new_position_description;
+        salary = new_salary;
+        managed_by = nullptr;
+        subordinates = nullptr;
+        department = new_department;
+        level = new_level;
+    }
+};
+
+class Store_Manager : public Employee
+{
+protected:
+    vector<CEO> *managed_by;
+    vector<Assistant_Store_Manager> *subordinates;
+
+public:
+    Store_Manager(string new_name, string new_position, string new_position_description, int new_salary, string new_department, int new_level)
+    {
+        name = new_name;
+        position = new_position;
+        position_description = new_position_description;
+        salary = new_salary;
+        managed_by = nullptr;
+        subordinates = nullptr;
+        department = new_department;
+        level = new_level;
+    }
+};
+
+class Assistant_Store_Manager : public Employee
+{
+protected:
+    vector<Store_Manager> *managed_by;
+    vector<Department_Manager> *subordinates;
+
+public:
+    Assistant_Store_Manager(string new_name, string new_position, string new_position_description, int new_salary, string new_department, int new_level)
+    {
+        name = new_name;
+        position = new_position;
+        position_description = new_position_description;
+        salary = new_salary;
+        managed_by = nullptr;
+        subordinates = nullptr;
+        department = new_department;
+        level = new_level;
+    }
+};
+
+class Department_Manager : public Employee
+{
+protected:
+    vector<Assistant_Store_Manager> *managed_by;
+    vector<Employee> *subordinates;
+
+public:
+    Department_Manager(string new_name, string new_position, string new_position_description, int new_salary, string new_department, int new_level)
+    {
+        name = new_name;
+        position = new_position;
+        position_description = new_position_description;
+        salary = new_salary;
+        managed_by = nullptr;
+        subordinates = nullptr;
+        department = new_department;
+        level = new_level;
     }
 };
--- main.cpp
+++ main.cpp
@@ -43,6 +43,90 @@ Employee create_employee_from_data(string string_of_data)
     return new_employee;
 }
 
+CEO create_CEO_from_data(string string_of_data)
+{
+    vector<string> class_data;
+    stringstream words(string_of_data);
+    string data;
+    while (getline(words, data, ','))
+    {
+        class_data.push_back(data);
+    }
+
+    string name = class_data[0];
+    string position = class_data[1];
+    string position_description = class_data[2];
+    int salary = stoi(class_data[3]);
+    string department = class_data[6];
+    int level = stoi(class_data[7]);
+
+    CEO new_CEO = CEO(name, position, position_description, salary, department, level);
+    return new_CEO;
+}
+
+Store_Manager create_Store_Manager_from_data(string string_of_data)
+{
+    vector<string> class_data;
+    stringstream words(string_of_data);
+    string data;
+    while (getline(words, data, ','))
+    {
+        class_data.push_back(data);
+    }
+
+    string name = class_data[0];
+    string position = class_data[1];
+    string position_description = class_data[2];
+    int salary = stoi(class_data[3]);
+    string department = class_data[6];
+    int level = stoi(class_data[7]);
+
+    Store_Manager new_Store_Manager = Store_Manager(name, position, position_description, salary, department, level);
+    return new_Store_Manager;
+}
+
+Assistant_Store_Manager create_Assistant_Store_Manager_from_data(string string_of_data)
+{
+    vector<string> class_data;
+    stringstream words(string_of_data);
+    string data;
+    while (getline(words, data, ','))
+    {
+        class_data.push_back(data);
+    }
+
+    string name = class_data[0];
+    string position = class_data[1];
+    string position_description = class_data[2];
+    int salary = stoi(class_data[3]);
+    string department = class_data[6];
+    int level = stoi(class_data[7]);
+
+    Assistant_Store_Manager new_Assistant_Store_Manager = Assistant_Store_Manager(name, position, position_description, salary, department, level);
+    return new_Assistant_Store_Manager;
+}
+
+Department_Manager create_Department_Manager_from_data(string string_of_data)
+{
+    vector<string> class_data;
+    stringstream words(string_of_data);
+    string data;
+    while (getline(words, data, ','))
+    {
+        class_data.push_back(data);
+    }
+
+    string name = class_data[0];
+    string position = class_data[1];
+    string position_description = class_data[2];
+    int salary = stoi(class_data[3]);
+    string department = class_data[6];
+    int level = stoi(class_data[7]);
+
+    Department_Manager new_Department_Manager = Department_Manager(name, position, position_description, salary, department, level);
+    return new_Department_Manager;
+}
+
 int main()
 {
     vector<string> rows_of_data = read_data();
